7e4265b6f71a3879ade1625aae9600c621778db0
kzfly1 <qichenxu47@gmail.com>
2024-09-26 20:21:01 +1000

    feat: add contact us page

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req,res)=>{
     res.send('Hello World')
 });
 
+app.get('/contact', (req,res)=>{
+    res.send('Contact Us')
+});
+
 app.listen(8080,()=>{
     console.log('listening at port 8080.')
 });
